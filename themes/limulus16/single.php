1239a87d6a52ab0086c04afd54edd4e5f7f6cde3
John Falcon <john@pixel-muffin.com>
2017-03-06 02:46:05 -0600

    Related articles in single look and feel

--- themes/limulus16/single.php
+++ themes/limulus16/single.php
@@ -1,12 +1,13 @@
-	<?php 
-		get_header();
-		$categories = get_the_category();
-		$cat_id = NULL;
-		foreach ($categories as $mycat)
-			$cat_id = ($mycat->parent === 0) ? $mycat->term_id : NULL;
-			
-		$cat_id = ($cat_id) ? $cat_id : '0';
-		the_post(); ?>
+<?php 
+	get_header();
+	the_post();
+	$categories = get_the_category();
+	$cat_id = NULL;
+
+	foreach ($categories as $mycat)
+		$cat_id = ($mycat->parent === 0) ? $mycat->term_id : NULL;
+		
+	$cat_id = ($cat_id) ? $cat_id : '0'; ?>
 
 		<section class="feed_container clearfix">
 			<article class="single_post">
@@ -47,35 +48,48 @@
 				<section class="mas_posts">
 					<h4 class=""><?php _e('Artículos relacionados', 'limulus'); ?></h4>
 
-				<?php
+			<?php
+				// Related posts
+				$args = array(
+					'posts_per_page'   => 4,
+					'post_type'        => 'post',
+					'post_status'      => 'publish',
+					'exclude'		   => $post->ID,
+					'cat'         	   => $cat_id
+				);
 
-					// Related posts
-					$args = array(
-						'posts_per_page'   => 4,
-						'post_type'        => 'post',
-						'post_status'      => 'publish',
-						'exclude'		   => $post->ID,
-						'cat'         	   => $cat_id
-					);
-
-					$related_posts = get_posts( $args );
+				$related_posts = get_posts( $args );
 
 				foreach ($related_posts as $related) : ?>
-
-					<article class="each_post">
-						<a href="<?php echo get_permalink( $related->ID ); ?>">
-							<?php echo get_the_post_thumbnail( $related->ID, 'thumbnail' ); ?>
-							<h3><?php echo qtrans_use(qtrans_getLanguage(), $related->post_title, false); ?></h3>
-						</a>
-					</article><!-- each_post -->
 					
-			<?php endforeach; ?>
+					<article class="each_post search_result">
+						<a rel="nofollow" href="<?php echo get_permalink( $related->ID ); ?>">
+							<?php 
+							if( has_post_thumbnail($related) ) {
+								echo get_the_post_thumbnail( $related->ID, 'thumbnail' );
+							}else{
+
+							 	echo "<img src='".THEMEPATH."/images/no_image.png' /> "; 
+							} ?>
+						</a>
+						<section class="info_post">
+							<div class="post_content">
+								<a href="<?php echo get_permalink( $related->ID ); ?>">
+									<h3><strong><?php echo qtrans_use(qtrans_getLanguage(), $related->post_title, false); ?></strong></h3>
+								</a>
+							</div>
+						</section>
+						
+					</article>
+					
+			<?php 
+				endforeach; ?>
 
 				</section>
 
-			</article><!-- each_post -->
+			</article>
 
-		</section><!-- feed_container -->
+		</section>
 		
 
-	<?php get_footer(); ?>
+<?php get_footer(); ?>
